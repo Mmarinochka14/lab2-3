85ac082bb5548b4589c7677ce69796bda4a4a010
Mmarinochka14 <marinkilll14@gmail.com>
2023-10-10 20:18:02 +0300

    Changes

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main() {
     setlocale(LC_ALL, "Rus");
     std::cout << "Привет, мир!" << std::endl;
+
+    std::cout << "Введите ваше имя: ";
+    std::string name;
+    std::cin >> name;
+
+    std::cout << "Привет, " << name << "!" << std::endl;
     return 0;
 }
